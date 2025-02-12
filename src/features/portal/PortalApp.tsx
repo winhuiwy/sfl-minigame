d8b452079d2dbb30c7b8b7da4591c5e48b92e37b
Winnie <winniehui.99@gmail.com>
2025-02-12 15:48:53 +0800

    remove auth (inside PortalExample) and added test image overlay

--- src/features/portal/PortalApp.tsx
+++ src/features/portal/PortalApp.tsx
@@ -1,19 +1,14 @@
-import { WalletProvider } from "features/wallet/WalletProvider";
 import React from "react";
 import { PortalProvider } from "./minigame/lib/PortalProvider";
-import { PortalExample } from "./minigame/PortalExample";
 import { MinigameBackground } from "./minigame/PortalBackground";
+import { TestGame } from "./minigame/TestGame";
 
 export const PortalApp: React.FC = () => {
   return (
-    // WalletProvider - if you need to connect to a players wallet
-    <WalletProvider>
-      {/* PortalProvider - gives you access to a xstate machine which handles state management */}
-      <PortalProvider>
-        <MinigameBackground>
-          <PortalExample />
-        </MinigameBackground>
-      </PortalProvider>
-    </WalletProvider>
+    <PortalProvider>
+      <MinigameBackground>
+        <TestGame />
+      </MinigameBackground>
+    </PortalProvider>
   );
 };
---
+++ src/features/portal/minigame/TestGame.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import silhouette from "/src/assets/bumpkins/silhouette.png";
+
+export const TestGame: React.FC = () => {
+  return (
+    <div>
+      <img src={silhouette} alt="Silhouette" />
+    </div>
+  );
+};
